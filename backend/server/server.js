c136daa71d796209c583e4c30b6d0213bab1374f
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-01-22 23:58:32 -0500

    Add get weather endpoint to backend

--- backend/server/server.js
+++ backend/server/server.js
@@ -10,6 +10,10 @@ app.get('/', async(req, res) => {
     res.json({ message: "Server is healthy!"})
 });
 
+app.get('/weather', async (req, res) => {
+
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
 });
