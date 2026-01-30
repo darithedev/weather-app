030dca1dfbeac8b6e3b9777b436f32e2101d75ac
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-01-30 18:27:09 -0500

    Add two useState unit initialized with imperial and days initalized with 7, add dailyForecast function to return daily forecast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 function App() {
   const [city, setCity] = useState("");
   const [result, setResult] = useState(null);
-  const [unit, setUnit] = useState("imperial")
+  const [unit, setUnit] = useState("imperial");
+  const [days, setDays] = useState(7)
 
   const dailyWeather = () => {
     const url = `http://localhost:8080/api/weather/${city}?units=${unit}`;
@@ -15,6 +16,17 @@ function App() {
         setCity(result.weather[0].name);
         setResult(result);
       });
+  }
+
+  const dailyForecast = () => {
+    const url = `http://localhost:8080/api/forecast/daily/${city}/${days}?units=${unit}`;
+
+    fetch(url)
+      .then((result) => response.json())
+      .then((result) => {
+        setCity(result.weather[0].name);
+        setResult(result);
+      })
   }
 
   return (
