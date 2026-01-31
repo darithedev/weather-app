3e4ef587e03810b849c371ffd0dadb6128e501e9
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-01-30 19:31:40 -0500

    Change cors origin to 5173

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = 8080;
 
 app.use(cors({
-    origin: 'http://localhost:3000' // allow only frontend to connect
+    origin: 'http://localhost:5173' // allow only frontend to connect
 }));
 
 app.use(express.json());
